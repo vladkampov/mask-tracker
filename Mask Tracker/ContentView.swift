77da1b1240ccfda16d1b34bcb168e3977c558fa2
vladkampov <vladyslavkampov@gmail.com>
2020-10-10 12:27:10 +0300

    Initial Commit

---
+++ Mask Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mask Tracker
+//
+//  Created by Vladyslav Kampov on 10.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mask Tracker/Mask_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mask_TrackerApp.swift
+//  Mask Tracker
+//
+//  Created by Vladyslav Kampov on 10.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct Mask_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
